abf30a296b319ee57bec3f8a3b8321e60d042bf9
Ivan Shipilov <ivanshipilov@gmail.com>
2023-04-06 15:56:37 +0300

    ...

--- resources/js/components/Notepad.vue
+++ resources/js/components/Notepad.vue
@@ -20,13 +20,14 @@
 							<div class="form-group">
 								<textarea v-model="input" class="form-control" id="notepad-text" name="text" rows="6" :disabled="waiting"></textarea>
 							</div>
-							<button type="button" class="btn btn-success btn-sm" :disabled="waiting || !(selected || input)" @click.prevent="save">Save</button>
-							<button type="button" class="btn btn-primary btn-sm" :disabled="waiting || !input" @click.prevent="store">Save as new</button>
-							<button type="button" class="btn btn-secondary btn-sm" :disabled="waiting || !input" @click.prevent="clear">Clear</button>
+							<button v-if="selected && !input" type="button" class="btn btn-danger btn-sm" :disabled="waiting" @click.prevent="save">Delete</button>
+							<button v-else type="button" class="btn btn-success btn-sm" :disabled="waiting || !input || (selected && selected.text == input)" @click.prevent="save">Save</button>
+							<button v-if="selected && input && selected.text != input" type="button" class="btn btn-primary btn-sm" :disabled="waiting" @click.prevent="store">Save as new</button>
+							<button v-if="input" type="button" class="btn btn-secondary btn-sm" :disabled="waiting" @click.prevent="clear">Clear</button>
 						</form>
 					</div>
 					<div class="list-group list-group-flush rounded">
-						<template v-for="note in notepad" v-key="note.id">
+						<template v-for="note in orderedByDesc" v-key="note.id">
 							<button type="button" class="list-group-item list-group-item-action text-truncate" :class="{ active: selected && selected.id == note.id }" :disabled="waiting" @click.prevent="select(note)">{{ note.text }}</button>
 						</template>
 					</div>
@@ -53,6 +54,11 @@ export default {
 			error: null
 		}
   },
+	computed: {
+		orderedByDesc: function () {
+			return _.orderBy(this.notepad, ['id'], ['desc'])
+		}
+	},
   created: function () { this.id = this.$options.name + this._uid },
   mounted: function () {  },
   methods: {
@@ -66,7 +72,6 @@ export default {
 		select: function (note) {
 			if (this.selected && this.selected.id == note.id) {
 				this.selected = null
-				this.input = ""
 			} else {
 				this.selected = note
 				this.input = note.text
